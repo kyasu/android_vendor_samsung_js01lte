f8dcc18650ae9334948b1297d99ab8b106361997
kyasu <kyasu.dev@gmail.com>
2016-11-23 09:23:33 +0900

    js01lte: Update Widevine blobs from bullhead

--- js01lte-vendor.mk
+++ js01lte-vendor.mk
@@ -301,7 +301,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/js01lte/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
     vendor/samsung/js01lte/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
     vendor/samsung/js01lte/proprietary/vendor/lib/libdisp-aba.so:system/vendor/lib/libdisp-aba.so \
-    vendor/samsung/js01lte/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
     vendor/samsung/js01lte/proprietary/vendor/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
     vendor/samsung/js01lte/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
     vendor/samsung/js01lte/proprietary/vendor/lib/libdrmtime.so:system/vendor/lib/libdrmtime.so \
